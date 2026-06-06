11e5700e395c4732391ed8ea42fa7de729f1b1aa
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-06-06 17:22:12 +0200

    finished up app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var birthday = 4
+        birthday += 10
+        birthday *= 3
+        birthday -= 5
+        birthday /= 2
+        Log.d("final answer", "answer $birthday")
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
