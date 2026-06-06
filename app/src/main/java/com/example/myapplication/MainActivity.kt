3f94bd8c294c6527d3ceadebb4c7fdeae3a93cac
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-06-06 22:50:47 +0200

    added spinner

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +25,27 @@ class MainActivity : AppCompatActivity() {
         birthday -= 5
         birthday /= 2
         Log.d("final answer", "answer $birthday")
+        val result = findViewById<TextView>(R.id.result)
+        result.text = birthday.toString()
+        val dropDown = findViewById<AutoCompleteTextView>(R.id.dropDown)
+        val items = listOf<String>(
+            "Water",
+            "Fire",
+            "Earth",
+            "Air"
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,
+            items)
+
+        dropDown.setAdapter(adapter)
+        val spinDown = findViewById<Spinner>(R.id.spinDown)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinDown.adapter = adapter
+
+
+
+
+
 
 
 
