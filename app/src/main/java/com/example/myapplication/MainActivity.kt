872901f1221663ea88bf1a8e82ecaba7804d0b2c
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-06-06 23:09:37 +0200

    turn a text into uppercase

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -19,26 +18,34 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textView)
+        val pop = "Hello World!"
+        // turn a text in to uppercase
+        textView.text = pop.uppercase()
+        // accumulating a value
         var birthday = 4
         birthday += 10
         birthday *= 3
         birthday -= 5
         birthday /= 2
+        // displaying the value with a log
         Log.d("final answer", "answer $birthday")
         val result = findViewById<TextView>(R.id.result)
         result.text = birthday.toString()
-        val dropDown = findViewById<AutoCompleteTextView>(R.id.dropDown)
-        val items = listOf<String>(
+        // creating a list of items to use into the spinner
+        val items = listOf(
+            "Choose an option",
             "Water",
             "Fire",
             "Earth",
             "Air"
         )
+        // creating the adapter variable that will be used to populate the spinner
         val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,
             items)
-
-        dropDown.setAdapter(adapter)
+        // declaring the spinner variable
         val spinDown = findViewById<Spinner>(R.id.spinDown)
+        // setting the adapter to the spinner
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinDown.adapter = adapter
 
